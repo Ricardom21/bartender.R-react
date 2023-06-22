39a062449bd15eb83a0d50ec89862d94f3f23357
ricardo.m21 <ricardoabraham00@gmail.com>
2023-06-21 21:35:52 -0300

    agregando navegavilidad en el carrito

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import { FiShoppingCart } from "react-icons/fi";
+import { CartContext } from '../../context/ShoppingCartContext';
+
 const CartWidget = () => {
-    const numeroItems = 5
+  const { getTotalItems } = useContext(CartContext);
+  const numeroItems = getTotalItems();
+
   return (
     <div className="flex items-center">
-    <FiShoppingCart className="mr-2 text-white text-xl" />
-    {numeroItems > 0 && (
-      <span className="bg-red-500 text-white rounded-full px-1 py-1 text-xs">
-        {numeroItems}
-      </span>
-    )}
-  </div>
-  )
+      <FiShoppingCart className="mr-2 text-white text-xl" />
+      {numeroItems > 0 && (
+        <span className="bg-red-500 text-white rounded-full px-1 py-1 text-xs">
+          {numeroItems}
+        </span>
+      )}
+    </div>
+  );
 }
 
-export default CartWidget
+export default CartWidget;
--- src/context/ShoppingCartContext.jsx
+++ src/context/ShoppingCartContext.jsx
@@ -1,26 +1,32 @@
-import { createContext, useState } from 'react';
+import React, { createContext, useState } from 'react';
 
-export const CartContext = createContext({cart:[]});
+export const CartContext = createContext({ cart: [] });
+
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
 
-  console.log(cartItems)
+  console.log(cartItems);
 
-  const addItems = (item, quantity)=>{
-    if(!isInCart(item.id)){
-      setCartItems(prev => [...prev, {item, quantity}])
-    } else{
-      console.error("NO SE PUEDE AGREGAR MAS O NO ANDA")
+  const addItems = (item, quantity) => {
+    if (!isInCart(item.id)) {
+      setCartItems((prev) => [...prev, { item, quantity }]);
+    } else {
+      console.error("NO SE PUEDE AGREGAR MAS O NO ANDA");
     }
-  }
-  // Aquí puedes definir las funciones para agregar, eliminar, actualizar elementos del carrito, etc.
+  };
 
-  const isInCart = (ItemId) => {
-    return cartItems.some(i=>i.id === ItemId)
-  }
+  const isInCart = (itemId) => {
+    return cartItems.some((i) => i.item.id === itemId);
+  };
+
+  const getTotalItems = () => {
+    return cartItems.reduce((total, item) => total + item.quantity, 0);
+  };
 
   return (
-    <CartContext.Provider value={{ cartItems, setCartItems, addItems, isInCart }}>
+    <CartContext.Provider
+      value={{ cartItems, setCartItems, addItems, isInCart, getTotalItems }}
+    >
       {children}
     </CartContext.Provider>
   );
