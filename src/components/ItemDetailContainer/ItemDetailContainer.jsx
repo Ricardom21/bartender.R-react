05577f8106bc139a555ae265acc20db83e31cb1d
ricardo.m21 <ricardoabraham00@gmail.com>
2023-06-30 22:48:53 -0300

    agregando base de datos

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,24 +1,36 @@
-import {ItemDetail} from '../ItemDetail/ItemDetail';
 import { useEffect, useState } from 'react';
-import { getProductsById } from '../../Mocks/products';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../index.js';
 import { useParams } from 'react-router-dom';
+import { ItemDetail } from '../ItemDetail/ItemDetail';
 
 export const ItemDetailContainer = () => {
-  const [product, setProduct] = useState (null)
-  const { productosId } = useParams()
-    useEffect(()=>{
-      getProductsById(productosId)
-        .then((response)=>setProduct(response))
-    },[productosId])
+  const [product, setProduct] = useState(null);
+  const { productosId } = useParams();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const productDocRef = doc(db, 'productos', productosId);
+        const productDocSnapshot = await getDoc(productDocRef);
+
+        if (productDocSnapshot.exists()) {
+          const productData = productDocSnapshot.data();
+          setProduct(productData);
+        } else {
+          console.log('El producto no existe.');
+        }
+      } catch (error) {
+        console.error('Error al obtener el producto:', error);
+      }
+    };
+
+    fetchProduct();
+  }, [productosId]);
+
   return (
     <div>
-      {
-        product?
-      <ItemDetail {...product}/>
-    :
-    <div> Loading</div>
-    }
+      {product ? <ItemDetail {...product} /> : <div>Loading</div>}
     </div>
-  )
+  );
 };
-
